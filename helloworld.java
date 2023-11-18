ff645cccc1b1511b3efe1b897ec9b398e0b4b915
tamilarasilaksh <150312904+tamilarasilaksh@users.noreply.github.com>
2023-11-18 17:17:05 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, my batch32 students!"); 
+        System.out.println("Hello, my batch32 Tamilarasi!"); 
     }
 }
